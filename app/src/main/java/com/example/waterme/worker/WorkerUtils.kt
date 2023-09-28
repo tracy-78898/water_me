f2977e299bfff38ad25aa31d8f14c3907c00b1f7
tracy-78898 <tdurha3@live.spcollege.edu>
2023-09-27 19:15:59 -0500

    Water_Me! with amended notification handling

---
+++ app/src/main/java/com/example/waterme/worker/WorkerUtils.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.waterme.worker
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.waterme.CHANNEL_ID
+import com.example.waterme.MainActivity
+import com.example.waterme.NOTIFICATION_ID
+import com.example.waterme.NOTIFICATION_TITLE
+import com.example.waterme.R
+import com.example.waterme.REQUEST_CODE
+import com.example.waterme.VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION
+import com.example.waterme.VERBOSE_NOTIFICATION_CHANNEL_NAME
+
+fun makePlantReminderNotification(message: String, context: Context) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        // Create the NotificationChannel (on API 26+ only)
+        //set high importance level
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        //create notification channel
+
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            VERBOSE_NOTIFICATION_CHANNEL_NAME,
+            importance
+        )
+        channel.description = VERBOSE_NOTIFICATION_CHANNEL_DESCRIPTION
+
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+
+        notificationManager?.createNotificationChannel(channel)
+    }
+
+    // Try-Catch block
+    try {
+        // Create PendingIntent
+        val pendingIntent: PendingIntent = createPendingIntent(context)
+
+        // Create the notification
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(NOTIFICATION_TITLE)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVibrate(LongArray(0))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        // Send out the notification
+        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, builder.build())
+    } catch (e: SecurityException) {
+        //  catch any SecurityException errors
+        // Log the error for debugging
+        Log.e("NotificationError", "Permission error when trying to show notification: ${e.message}")
+    }
+
+}
+
+fun createPendingIntent(appContext: Context): PendingIntent {
+    val intent = Intent(appContext, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+
+    // Flag to detect unsafe launches of intents for Android 12 and higher
+    var flags = PendingIntent.FLAG_UPDATE_CURRENT
+    flags = flags or FLAG_IMMUTABLE
+
+    return PendingIntent.getActivity(
+        appContext,
+        REQUEST_CODE,
+        intent,
+        flags
+    )
+}
+
+
